1eb4fe4d470c8a0adb4795819d42c94f8d6752da
Patrascu Hary <harypatrascu.1@gmail.com>
2024-12-28 16:51:30 +0200

    Item model update and new migration created.

--- ASP.NET Core MVC/Migrations/20241228145045_Second migration.Designer.cs
+++ ASP.NET Core MVC/Migrations/20241228145045_Second migration.Designer.cs
@@ -3,6 +3,7 @@ using ASP.NET_Core_MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASP.NET_Core_MVC.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20241228145045_Second migration")]
+    partial class Secondmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ASP.NET Core MVC/Models/Item.cs
+++ ASP.NET Core MVC/Models/Item.cs
@@ -5,6 +5,7 @@
         #region Private properties
         private int _id;
         private string _name;
+        private double _price;
         #endregion
 
         #region Getters and Setters
@@ -17,6 +18,11 @@
         {
             get => _name;
             set => _name = value;
+        }
+        public double Price
+        {
+            get => _price;
+            set => _price = value;
         }
         #endregion
     }
