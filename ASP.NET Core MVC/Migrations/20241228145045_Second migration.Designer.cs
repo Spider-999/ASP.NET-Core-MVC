c41452274fd640814693bcf5016dbf2a7a35da78
Patrascu Hary <harypatrascu.1@gmail.com>
2024-12-28 16:54:37 +0200

    Update-Database

--- ASP.NET Core MVC/Migrations/20241228145045_Second migration.Designer.cs
+++ ASP.NET Core MVC/Migrations/20241228145045_Second migration.Designer.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using AppContext = ASP.NET_Core_MVC.Data.AppContext;
 
 #nullable disable
 
--- ASP.NET Core MVC/Migrations/AppContextModelSnapshot.cs
+++ ASP.NET Core MVC/Migrations/AppContextModelSnapshot.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using AppContext = ASP.NET_Core_MVC.Data.AppContext;
 
 #nullable disable
 
