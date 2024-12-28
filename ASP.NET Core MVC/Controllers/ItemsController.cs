715a231ec0828cf0c2d3294ea2987fe9c0aabb68
Patrascu Hary <harypatrascu.1@gmail.com>
2024-12-28 14:20:12 +0200

    Added a new controller, model and view.

---
+++ ASP.NET Core MVC/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using ASP.NET_Core_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_MVC.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Id = 1, Name = "Item 1" };
+            return View(item);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content(id.ToString());
+        }
+    }
+}
---
+++ ASP.NET Core MVC/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿namespace ASP.NET_Core_MVC.Models
+{
+    public class Item
+    {
+        #region Private properties
+        private int _id;
+        private string _name;
+        #endregion
+
+        #region Getters and Setters
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        #endregion
+    }
+}
