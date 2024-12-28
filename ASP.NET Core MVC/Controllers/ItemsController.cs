5f2ac47fd7247251f04e74f160e73df696cadd7d
Patrascu Hary <harypatrascu.1@gmail.com>
2024-12-28 18:15:19 +0200

    Added update functionality.

--- ASP.NET Core MVC/Controllers/ItemsController.cs
+++ ASP.NET Core MVC/Controllers/ItemsController.cs
@@ -28,6 +28,11 @@ namespace ASP.NET_Core_MVC.Controllers
             return View(item);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         /// <summary>
         /// Create a new item and add it to the database.
         /// Bind the item we get from the form to the item object.
@@ -43,6 +48,36 @@ namespace ASP.NET_Core_MVC.Controllers
                 // Save the changes to the database
                 await _context.SaveChangesAsync();
                 // Redirect the user to the index page after the item is added
+                return RedirectToAction(nameof(Index));
+            }
+            // Otherwise keep the user on the same page
+            return View(item);
+        }
+
+        /// <summary>
+        /// Find an item in the database.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Edit(int id)
+        {
+            // Find the item in the database
+            var item = await _context.Items.FirstOrDefaultAsync(item => item.Id == id);
+            // Return the view with the item
+            return View(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id", "Name", "Price")] Item item)
+        {
+            // Check if the item we get is a valid item.
+            if (ModelState.IsValid)
+            {
+                // Update the item in the database
+                _context.Update(item);
+                // Save the changes to the database
+                await _context.SaveChangesAsync();
+                // Redirect the user to the index page after the item is updated
                 return RedirectToAction(nameof(Index));
             }
             // Otherwise keep the user on the same page
